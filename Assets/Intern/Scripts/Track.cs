0591fa35fe7fb95d13d5f01e70c6959d1d8c38a7
DieSoftwareentwickler <info@diesoftwareentwickler.de>
2017-05-20 12:07:20 +0200

    track fade

--- Assets/Intern/Scripts/Track.cs
+++ Assets/Intern/Scripts/Track.cs
@@ -4,95 +4,149 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-namespace Assets.Intern.Scripts
+/// <summary>
+/// Generates a track
+/// </summary>
+public class Track : MonoBehaviour
 {
-	public class Track: MonoBehaviour
+	[SerializeField]
+	private GameObject left_anchor;
+	[SerializeField]
+	private GameObject right_anchor;
+	[SerializeField]
+	private GameObject center;
+	[SerializeField]
+	private Material material;
+
+	private List<Vector3> vertices = new List<Vector3>();
+	private List<int> indieces = new List<int>();
+	private List<Vector2> uv = new List<Vector2>();
+	private Vector3[] normal = new Vector3[ 0 ];
+
+	private float last_update;
+	private int y_uv;
+
+	/// <summary>
+	/// Generates a new sement
+	/// </summary>
+	private void update_track()
 	{
-		[SerializeField]
-		private GameObject left_anchor;
-		[SerializeField]
-		private GameObject right_anchor;
-		[SerializeField]
-		private GameObject center;
-		[SerializeField]
-		private Material material;
-
-		private List<Vector3> vertices = new List<Vector3>();
-		private List<int> indieces = new List<int>();
-		private List<Vector2> uv = new List<Vector2>();
-
-		private float last_update;
-		private int y_uv;
-
-		private void update_track() {
-			foreach( Transform child in transform )
-			{
-				DestroyImmediate( child.gameObject );
-			}
-
-			List<Vector3> vertices = this.vertices;
-
-			if ( 100 < vertices.Count )
-			{
-				vertices.RemoveRange( 0 , 2 );
-				uv.RemoveRange( 0 , 2 );
-				//indieces.RemoveRange( 0 , 6 );
-			}
-
-			vertices.Add( left_anchor.transform.position );
-			vertices.Add( right_anchor.transform.position );
-			uv.Add( new Vector2( 0 , y_uv ) );
-			uv.Add( new Vector2( 1 , y_uv ) );
-			y_uv = 0 >= y_uv ? 1 : 0;
-
-			if ( 4 > vertices.Count )
-			{
-				return;
-			}
-
-			if ( 150 > vertices.Count )
-			{
-				int count = vertices.Count;
-				indieces.Add( count - 2 );
-				indieces.Add( count - 4 );
-				indieces.Add( count - 3 );
-				indieces.Add( count - 2 );
-				indieces.Add( count - 3 );
-				indieces.Add( count - 1 );
-			}
-
-			GameObject obj = new GameObject();
-			MeshFilter filter = obj.AddComponent<MeshFilter>();
-			Mesh mesh = new Mesh();
-			mesh.name = "track";
-
-			mesh.vertices = vertices.ToArray();
-			mesh.triangles = indieces.ToArray();
-			mesh.uv = uv.ToArray();
-
-			filter.mesh = mesh;
-
-			obj.transform.SetParent( transform , true );
-			obj.transform.localScale = Vector3.one;
-			obj.transform.rotation = Quaternion.Euler( new Vector3( 0 , 0 , 0 ) );
-
-			MeshRenderer renderer = obj.AddComponent<MeshRenderer>();
-			renderer.material = material;
-
-			this.vertices = vertices;
-		}
-
-		private void Update()
+		foreach ( Transform child in transform )
 		{
-			if ( Time.time - 0.2 > last_update )
-			{
-				last_update = Time.time;
-
-				update_track();
-			}
-
-			material.SetVector( "_StartPosition" , ( left_anchor.transform.position + right_anchor.transform.position ) / 2 );
+			DestroyImmediate( child.gameObject );
 		}
 
+		List<Vector3> vertices = this.vertices;
+
+		if ( 100 < vertices.Count )
+		{
+			vertices.RemoveRange( 0 , 2 );
+			uv.RemoveRange( 0 , 2 );
+		}
+
+		// appendd vertices
+		vertices.Add( left_anchor.transform.position );
+		vertices.Add( right_anchor.transform.position );
+		int count = vertices.Count;
+
+		// append uvs
+		uv.Add( new Vector2( 0 , y_uv ) );
+		uv.Add( new Vector2( 1 , y_uv ) );
+		y_uv = 0 >= y_uv ? 1 : 0;
+
+		if ( 4 > vertices.Count )
+		{
+			return;
+		}
+
+		if ( 100 >= vertices.Count )
+		{
+			// appand indieces
+			indieces.Add( count - 2 );
+			indieces.Add( count - 4 );
+			indieces.Add( count - 3 );
+			indieces.Add( count - 2 );
+			indieces.Add( count - 3 );
+			indieces.Add( count - 1 );
+		}
+
+		// recalculate normals
+		if ( normal.Length != count )
+		{
+			normal = generate_normal( count );
+		}
+
+		GameObject obj = new GameObject();
+		MeshFilter filter = obj.AddComponent<MeshFilter>();
+		Mesh mesh = new Mesh();
+		mesh.name = "track";
+
+		mesh.vertices = vertices.ToArray();
+		mesh.triangles = indieces.ToArray();
+		mesh.uv = uv.ToArray();
+		mesh.normals = normal;
+
+		filter.mesh = mesh;
+
+		obj.transform.SetParent( transform , true );
+		obj.transform.localScale = Vector3.one;
+		obj.transform.rotation = Quaternion.Euler( new Vector3( 0 , 0 , 0 ) );
+
+		MeshRenderer renderer = obj.AddComponent<MeshRenderer>();
+		renderer.material = material;
+
+		this.vertices = vertices;
+	}
+
+	private void Update()
+	{
+		float time = Time.time;
+		if ( time - 0.2 > last_update )
+		{
+			last_update = Time.time;
+
+			update_track();
+		}
+
+		material.SetFloat( "_Progress" , ( ( time - last_update ) / 0.2f ) );
+	}
+
+	/// <summary>
+	/// Regenerate normals for given vertex count
+	/// </summary>
+	/// <param name="vertext_count"></param>
+	/// <returns></returns>
+	private Vector3[] generate_normal( int vertext_count )
+	{
+		Vector3[] normal = new Vector3[ vertext_count ];
+		for ( int i = 0 ; i < vertext_count ; i++ )
+		{
+			normal[ i ] = Vector3.up;
+		}
+
+		if ( 6 < vertext_count )
+		{
+			// add end factor
+			float factor = 0.6f;
+			for ( int i = 0 ; i < 6 ; i += 2 )
+			{
+				normal[ i ].z = factor;
+				normal[ i + 1 ].z = factor;
+				factor -= 0.2f;
+			}
+
+			// add begin factor
+			factor = 0.6f;
+			for ( int i = 1 ; i <= 6 ; i += 2 )
+			{
+				normal[ vertext_count - i ].z = factor;
+				normal[ vertext_count - i - 1 ].z = factor;
+				normal[ vertext_count - i ].x = 1;
+				normal[ vertext_count - i - 1 ].x = 1;
+				factor -= 0.2f;
+			}
+		}
+
+		return normal;
 	}
 }
--- Assets/Intern/Shader/simple_color.shader
+++ Assets/Intern/Shader/simple_color.shader
@@ -2,7 +2,7 @@
 	Properties{
 		_MainTex( "Texture", 2D ) = "white" {}
 		_Color( "Color", Color ) = (1,1,1,1)
-		_StartPosition( "Vector", Vector ) = (1,1,1)
+		_Progress( "Range", Range( 0 , 1 ) ) = 1
 	}
 		SubShader{
 		Tags{ "RenderType" = "Opaque" }
@@ -11,23 +11,24 @@
 #pragma surface surf Lambert alpha vertex:vert
 	struct Input {
 		float2 uv_MainTex;
-		float3 customColor;
-		float dst;
+		float3 normal;
 	};
 	float3 _StartPosition;
 	void vert( inout appdata_full v, out Input o ) {
-		UNITY_INITIALIZE_OUTPUT( Input,o );
-		o.dst = distance( _StartPosition ,  v.vertex );
+		UNITY_INITIALIZE_OUTPUT( Input , o );
+		o.normal = v.normal;
 	}
 	sampler2D _MainTex;
 	float4  _Color;
+	float _Progress;
 	
 	void surf( Input IN, inout SurfaceOutput o ) {
 		float4 tex = tex2D( _MainTex, IN.uv_MainTex );
 		
 		_Color.a = 1;
 		o.Emission = tex.rgb * _Color;
-		o.Alpha = tex.a * min( 1 , pow( IN.dst / 30 , 20 ) );
+		
+		o.Alpha = tex.a * max( min( ( ( IN.normal.z - ( 0.2 + ( 1 - abs( IN.normal.x - _Progress ) ) * 0.2 ) ) * -1 ) * 5 , 1 ) , 0 );
 	}
 	ENDCG
 	}
