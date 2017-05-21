3742c355eac4ce0e9c4f30b4d45dc7bb2f888082
DieSoftwareentwickler <info@diesoftwareentwickler.de>
2017-05-21 21:34:58 +0200

    zeug

---
+++ Assets/Intern/Scripts/View/Intro.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Merge multiple quads to one big mesh
+/// </summary>
+class Intro : MonoBehaviour
+{
+	[SerializeField]
+	private Vector3 speed;
+	[SerializeField]
+	private float timeout;
+
+	private float start;
+
+	private void Start()
+	{
+		start = Time.time;
+	}
+
+	private void Update()
+	{
+		transform.position += speed * Time.deltaTime;
+
+		if ( Time.time - timeout > start )
+		{
+			SceneManager.LoadScene( "main" );
+		}
+	}
+}
